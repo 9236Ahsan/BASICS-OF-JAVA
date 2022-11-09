fb39a4668f4621e700420d9cd2338bb8258a9fd2
Mohd Ahsan <105874680+9236Ahsan@users.noreply.github.com>
2022-11-09 23:09:32 +0530

    Add files via upload

---
+++ Assis3.java
@@ -0,0 +1,17 @@
+// Write the program to print sum, multiply, divide and subtraction of two
+// numbers using constant values
+
+public class Assis3 {
+    public static void main(String args[]){
+        int x=4;
+        int y=2;
+
+        
+        System.out.println(" sum of "+x+" And "+y+" is "+(x+y));
+        System.out.println(" Multiply of "+x+" And "+y+" is "+(x*y));
+        System.out.println(" Divide of "+x+" And "+y+" is "+(x/y));
+        System.out.println(" Substraction of "+x+" And "+y+" is "+(x-y));
+
+    }
+    
+}
