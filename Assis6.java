c70b045426032768f867a5232b69a2220a801314
Mohd Ahsan <105874680+9236Ahsan@users.noreply.github.com>
2022-12-18 12:46:09 +0530

    Add files via upload
    assesment

---
+++ Assis6.java
@@ -0,0 +1,27 @@
+// Write the code to print student data using Scanner class.
+import java.util.*;
+class StudI{
+    String name;
+    String course;
+    String age;
+
+    public void StudInf(){
+     
+System.out.println(this.name);
+
+System.out.println(this.age);
+    }
+}
+public class Assis6{
+    public static void main(String args[]){
+        Student s1=new Student();
+        s1.name="Ahsan";
+        s1.age=18;
+
+        s1.StudInf();
+    }
+    }
+
+    
+    
+}
