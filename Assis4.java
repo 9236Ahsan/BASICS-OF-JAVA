68c973599a757cac6132fa343e7d18bfc5909ff9
Mohd Ahsan <105874680+9236Ahsan@users.noreply.github.com>
2022-12-18 12:41:34 +0530

    Add files via upload

---
+++ Assis4.java
@@ -0,0 +1,13 @@
+// Write the code to print area of circle using constant float values.
+
+public class Assis4 {
+    public static void main(String args[]){
+        float r=2;
+        double pi=3.14;
+
+        double Area_Of_Circle=((pi)*r*r);
+
+        System.out.println("The Area Of Circle is given  "+Area_Of_Circle);
+    }
+    
+}
